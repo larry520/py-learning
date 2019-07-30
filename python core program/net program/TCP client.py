af952d0c6984f714427bef339a7191775271622a
Trigle <44629016+larry520@users.noreply.github.com>
2019-07-30 17:57:37 +0800

    网络编程

---
+++ python core program/net program/TCP client.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# 文件名：TPC client.py
+import socket
+import sys
+
+# 获取主机名
+# host = socket.gethostname()
+host = '192.168.100.191'
+# 设置端口号
+port = 9999
+
+# 创建socket对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 连接服务， 指定主机和端口
+s.connect((host, port))
+
+while True:
+    msgsend = input(">>").strip()
+    if msgsend:
+        # 发送数据
+        s.send(msgsend.encode('utf-8'))
+    else:
+        break
+
+    # 接收小于 1024 字节的数据
+    msg = s.recv(1024)
+    print(msg.decode('utf-8'))
+
+# 关闭客户端
+s.close()
+
---
+++ python core program/net program/TCP server.py
@@ -0,0 +1,46 @@
+#  -*- encoding: utf-8 -*-
+# 文件名: TCP server.py
+
+import socket
+import sys
+import time
+
+# 创建 socket 对象
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取主机名
+host = socket.gethostname()
+# host = '192.168.1.1'
+port = 9999
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数, 超过后排队
+serversocket.listen(5)
+
+
+# 建立客户连接
+print('服务器启动，监听客户端链接')
+clientsocket, addr = serversocket.accept()
+print("连接客户端地址:%s" % str(addr))
+
+while True:
+    try:
+        # 接收客户端不大于1024 byte数据
+        msgrev = clientsocket.recv(1024)
+    except Exception:
+        print('断开客户端连接', addr)
+
+    if msgrev:
+        print('客户端发送内容：', msgrev.decode('utf-8'))
+    else:
+        break
+    msg = time.strftime("%Y-%m-%d %X")  # 获取结构化事件戳
+    msg1 = '[%s]:%s' % (msg, msgrev.decode('utf-8'))
+
+    # 发送数据
+    clientsocket.send(msg1.encode('utf-8'))  # 发送消息给已链接客户端
+
+clientsocket.close()
+
