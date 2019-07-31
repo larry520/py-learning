e0a21649284cda7f9c71bff3cb98f4f829d749be
Trigle <44629016+larry520@users.noreply.github.com>
2019-07-31 17:59:55 +0800

    mongoDB

--- python core program/MySQL.py
+++ python core program/MySQL.py
@@ -1,2 +1,14 @@
 # -*- encoding: utf-8 -*-
 
+import pymongo
+#
+# myclient = pymongo.MongoClient("mongodb://localhost:9999/")
+# mydb = myclient["runoobdb"]
+
+myclient = pymongo.MongoClient('mongodb://localhost:9999/')
+
+dblist = myclient.list_database_names()
+# dblist = myclient.database_names()
+if "runoobdb" in dblist:
+    print("数据库已存在！")
+
