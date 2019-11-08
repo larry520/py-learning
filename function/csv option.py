94babfbee3d7134a705fe5aa204d31bc32f495a6
larry520 <279504939@qq.com>
2019-11-08 09:16:03 +0800

    csv option update

--- function/csv option.py
+++ function/csv option.py
@@ -2,19 +2,37 @@
 # date: 2019年8月1日17:23:33
 
 import csv
+import os
+import datetime
 
-# 写
-with open("test.csv", "w", encoding='utf8', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["index", "a_name", "b_name"])
-    writer.writerows([[0, 'a1', 'b1'], [1, 'a2', 'b2'], [2, 'a3', 'b3']])
 
+fileName="weightData.csv"
+filepath = os.getcwd() + os.sep + "dota" + os.sep   # 在当前进程根目录下创建 dota 文件夹
+if not os.path.exists(filepath+fileName):
+    os.makedirs(filepath)
+    # 创建文件
+    with open(filepath+fileName, "w+", encoding='utf8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        head = ["index", "datetime", "Dispenser","pulsTime(ms)","cycleTime(ms)","dotNum","totalWeight","potWeight"]
+        writer.writerow(head)
+
+# print(os.getcwd())
 # 读
-with open("test.csv", "r") as csvfile:
+with open(fileName, "r+") as csvfile:
+    lines = csvfile.readlines()
+    lastLine = lines[-1].split(',')
+if lastLine[0] == "index":
+    index = 0
+else:
+    index = int(lastLine[0])
+
+# 追加 'a+' 添加到文件后
+with open(fileName, "a+", encoding='utf8', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    datas = [index + 1, datetime.datetime.now(),'a1', 'b1']
+    writer.writerow(datas)
+
+with open(fileName, "r+") as csvfile:
     readr = csv.reader(csvfile)
     for line in readr:
         print(line)
-
-import datetime
-
-print(datetime.datetime.now())
--- temp.py
+++ temp.py
@@ -10,45 +10,50 @@ import time,datetime
 #
 # a = np.repeat(np.arange(5).reshape([1,-1]),10,axis=0)
 # print(a)
+#
+# class demo(object):
+#
+#     def __init__(self):
+#         self.a = 10
+#         self.d = 1
+#         b = 20
+#     def test1(self,k):
+#         c = 30
+#         self.d = 40
+#         print(self.a,k)
+#     @property
+#     def d(self):
+#         return self.d
+#     @d.setter
+#     def d(self, value):
+#         sefl.d = value
+#     def test2(self):
+#         sum = self.a +self.d
+#
+#
+# A = demo
+# B = demo
+# A.d = 10
+# print(A.d)
+#
+# print(type(datetime.datetime.now().strftime("%H:%M:%S")))
+# print(type("good"))
+#
+# t1 = datetime.datetime.now().strftime("%Y-%m-%d") +" 23:01:00"
+# time.sleep(1)
+# t2 = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# if t1>t2:
+#     print(t1, t2)
+#
+# stra = "12:33,12:20"
+# strb = stra.split(',')
+# print(strb)
+# t = time.time()
+# t2 = datetime.datetime.now()
+# t3 = t - t2
+# print(t, t2, t3)
 
-class demo(object):
 
-    def __init__(self):
-        self.a = 10
-        self.d = 1
-        b = 20
-    def test1(self,k):
-        c = 30
-        self.d = 40
-        print(self.a,k)
-    @property
-    def d(self):
-        return self.d
-    @d.setter
-    def d(self, value):
-        sefl.d = value
-    def test2(self):
-        sum = self.a +self.d
-
-
-A = demo
-B = demo
-A.d = 10
-print(A.d)
-
-print(type(datetime.datetime.now().strftime("%H:%M:%S")))
-print(type("good"))
-
-t1 = datetime.datetime.now().strftime("%Y-%m-%d") +" 23:01:00"
-time.sleep(1)
-t2 = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-if t1>t2:
-    print(t1, t2)
-
-stra = "12:33,12:20"
-strb = stra.split(',')
-print(strb)
-t = time.time()
-t2 = datetime.datetime.now()
-t3 = t - t2
-print(t, t2, t3)
+a =3.1314526
+b = "%.3f" %float("%.5f" % a)
+print(b,type(b))
